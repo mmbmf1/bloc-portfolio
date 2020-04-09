54ada648506331dcb4c4386dd5b3dd9bf65593e6
Michael <mmbmf.mm@gmail.com>
2020-04-08 21:08:05 -0500

    updated data store and styling

---
+++ datastore.js
@@ -0,0 +1,49 @@
+const STORE = [
+  {
+    img_src: "",
+    project_title: "Ride Boost",
+    project_tech_txt_1: "PostgreSQL",
+    project_tech_txt_2: "Express.js",
+    project_tech: ["fab fa-react", "fab fa-node"],
+    project_description:
+      "Ride Boost a ride share driver companion app. The app gives drivers access to real-time weather data, arriving & departing flight information and local events near the driver.",
+    reop_link: "https://github.com/mmbmf1/rideboost-app",
+    live_link: "https://rideboost.now.sh/",
+  },
+
+  {
+    img_src: "",
+    project_title: "backpackd",
+    project_tech_txt_1: "PostgreSQL",
+    project_tech_txt_2: "Express.js",
+    project_tech: ["fab fa-react", "fab fa-node"],
+    project_description:
+      "backpackd allows a user to create and edit lists of backpacks. Items and their weight can be added to a backpack to calculate the total weight. Backpacks can also be deleted.",
+    reop_link: "https://github.com/mmbmf1/backpackd-app",
+    live_link: "https://backpackd.now.sh/",
+  },
+
+  {
+    img_src: "",
+    project_title: "Trail Buddy",
+    project_tech_txt_1: "",
+    project_tech_txt_2: "",
+    project_tech: ["fab fa-html5", "fab fa-css3", "fab fa-js"],
+    project_description:
+      "A user can input a location (city, state or zip code), and a list of 15 neaby trails will display with some brief info. A link to additional trail info and the 3 day forecast for the trail is also displayed.",
+    reop_link: "https://github.com/mmbmf1/api-hack",
+    live_link: "https://mmbmf1.github.io/api-hack/",
+  },
+
+  {
+    img_src: "",
+    project_title: "Quiz App",
+    project_tech_txt_1: "",
+    project_tech_txt_2: "",
+    project_tech: ["fab fa-html5", "fab fa-css3", "fab fa-js"],
+    project_description:
+      "The New Parent quiz app prompts users thorugh a set of questions with feedback about their score along the way. A summary of the complete quiz is given at the end.",
+    reop_link: "https://github.com/mmbmf1/quiz-app",
+    live_link: "https://mmbmf1.github.io/quiz-app/",
+  },
+];
--- index.js
+++ index.js
@@ -1,3 +1,66 @@
+//render projects
+function renderProjects() {
+  // console.log(STORE);
+  STORE.map((project) => {
+    // console.log(project.project_title);
+    $(".projects").append(`<div class="project-container">
+                <div class="img-container">
+                  <img
+                    id="app"
+                    src="${project.img_src}"
+                    alt="project screenshot"
+                  />
+                </div>
+                <div class="project-desc">
+                  <h3>${project.project_title}</h3>
+                  <ul class="tech-icons">
+                  ${project.project_tech
+                    .map((icon) => `<li><i class="${icon}"></i><li>`)
+                    .join("")}
+                    <li class='tech-txt'>${project.project_tech_txt_1}</li>
+                    <li class='tech-txt'>${project.project_tech_txt_2}</li>
+                  </ul>
+                  <p>
+                    ${project.project_description}
+                  </p>
+                  <div class="project-buttons">
+                    <a
+                      href="${project.reop_link}"
+                      target="_blank"
+                      class="button"
+                      >Code</a
+                    >
+                    <a
+                      href="${project.live_link}"
+                      target="_blank"
+                      class="button"
+                      >Quiz</a
+                    >
+                  </div>
+                </div>
+              </div>`);
+  });
+  $("li:empty").remove();
+}
+
+//fade in projects
+$(window).scroll(function () {
+  var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+  $(".project-container").each(function () {
+    var objectBottom = $(this).offset().top + $(this).outerHeight();
+
+    if (objectBottom - 500 < windowBottom) {
+      if ($(this).css("opacity") == 0) {
+        $(this).fadeTo(500, 1);
+      }
+    } else {
+      if ($(this).css("opacity") == 1) {
+        $(this).fadeTo(500, 0);
+      }
+    }
+  });
+});
+
 //function for home click
 function homeClick() {
   $(".home-link").on("click", function () {
@@ -24,7 +87,7 @@ function clickProjects() {
       "font-size": "12px",
       "font-family": "Hind, sans-serif",
     });
-    $(".bloc-projects").css("display", "block");
+    $(".bloc-projects").fadeIn(1000);
     $(".main-page").css("display", "none");
     $(".about-me").css("display", "none");
   });
@@ -48,7 +111,7 @@ function navClickProjects() {
       "font-size": "12px",
       "font-family": "Hind, sans-serif",
     });
-    $(".bloc-projects").css("display", "block");
+    $(".bloc-projects").fadeIn(1000);
     $(".main-page").css("display", "none");
     $(".about-me").css("display", "none");
   });
@@ -57,6 +120,7 @@ function navClickProjects() {
 //functions for learn more click
 function clickLearnMore() {
   $("#learn-more").on("click", function () {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
     $("#nav-learn-more").css({
       "border-bottom": "3px solid white",
       "font-size": "16px",
@@ -74,12 +138,13 @@ function clickLearnMore() {
     });
     $(".main-page").css("display", "none");
     $(".bloc-projects").css("display", "none");
-    $(".about-me").css("display", "block");
+    $(".about-me").fadeIn(1000);
   });
 }
 
 function navClickLearnMore() {
   $("#nav-learn-more").on("click", function () {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
     $("#nav-learn-more").css({
       "border-bottom": "3px solid white",
       "font-size": "16px",
@@ -97,12 +162,13 @@ function navClickLearnMore() {
     });
     $(".main-page").css("display", "none");
     $(".bloc-projects").css("display", "none");
-    $(".about-me").css("display", "block");
+    $(".about-me").fadeIn(1000);
   });
 }
 
 //run all portfolio functions
 function createPortfolio() {
+  renderProjects();
   clickProjects();
   navClickProjects();
   clickLearnMore();
